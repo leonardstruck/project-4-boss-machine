c7c8f557ef94781a5539d3061357bbf9f885cf1a
Leonard Struck <info@leonardstruck.de>
2018-08-05 22:01:27 +0200

    Add Ideas Router

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ const apiRouter = express.Router();
 
 const minionsRouter = require('./minionsrouter');
 apiRouter.use('/minions', minionsRouter);
-
+const ideasRouter = require('./ideasrouter');
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
